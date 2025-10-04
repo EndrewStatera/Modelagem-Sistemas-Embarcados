bb1aa084eb04e2f1ac8462e3317c640c01b4bc74
nexcoyocoatl <jukemaia@gmail.com>
2025-10-04 19:01:48 -0300

    adicionadas classes auxiliares

---
+++ ImageObject.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class ImageObject
+{
+    public ArrayList<Pair> edgePixels;
+
+    public ImageObject()
+    {
+        edgePixels = new ArrayList<Pair>();
+    }
+}
---
+++ Pair.java
@@ -0,0 +1,10 @@
+public class Pair {
+    public int x;
+    public int y;
+
+    public Pair(int _x, int _y)
+    {
+        this.x = _x;
+        this.y = _y;
+    }
+}
